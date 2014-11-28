3344eb717a9ea75d0e8b66f1d886f1c7d094e102
Daniel <djrenjiffo@terra.com.br>
2014-11-28 20:07:02 -0200

    Inicio

--- ep3.c
+++ ep3.c
@@ -1,58 +1,29 @@
-/*Como passo inicial para construçao do nosso
-ep3, vamo efetuar aqui uma multiplicacao de duas matrizes
-de forma otimizada para a linguagem c  ou seja atravez de linha
-e não coluna*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
-/*Prototipos*/
-FILE  *openSafe(char arquivo[]);
-float **pegaMatriz(FILE *arquivo, int n);
-void produtoMatriz(float **A,float **B, int n);
+#include <math.h>
+
+/*FUNÇÕES*/
+float **pegaMatriz(FILE *arquivo, int n, int m);
+float  *pegaVetor(FILE *arquivo,int n);
+FILE   *openSafe(char arquivo[]);
+float **buildQR(float **A, int n, int m);
+void rowUperTriang(float **A, float *b, int n);
+
+/*Mais importante*/
+float minionSqaureSolution(float **R,float *b,int n, int m);
+
 
 /*programa principal*/
 int main (int argc, char** argv) {
   /*Declarações*/
-  FILE *arquivo1;
-  FILE *arquivo2;
+  FILE *arquivo;
   float **A; 
-  float **B;
-  int n1;
-  int n2;
+  float *b;
+  int n;
+  int m;
   int x,y;
-  /*pega as matrizes*/
-  arquivo1 = openSafe(argv[1]);
-  arquivo2 = openSafe(argv[2]);
-  fscanf(arquivo1,"%d",&n1);
-  fscanf(arquivo2,"%d",&n2); 
-
-  
-  A = pegaMatriz(arquivo1, n1);
-  B = pegaMatriz(arquivo2, n2);
- 
-  
-   /*Imprime matriz Apenas teste*/
-   printf("\nPrimeira matriz\n" );
-   for(x = 0; x < n1; x++) {
-   	   for(y = 0; y < n1; y ++)
-   	   	printf("%f ",A[x][y]);
-
-       printf("\n"); 
-   }
-
-   /*Imprime matriz Apenas teste*/
-   printf("\nsegunda matriz\n" );
-   for(x = 0; x < n2; x++) {
-   	   for(y = 0; y < n2; y ++)
-   	   	printf("%f ",B[x][y]);
-
-       printf("\n"); 
-   }
-
-   printf("\n");
-   produtoMatriz(A,B,3);
-
+  float a;
+   
   return 0;
 }
 
@@ -70,33 +41,142 @@ FILE *openSafe(char arquivo[]) {
 }
 
 
-float **pegaMatriz(FILE *arquivo, int n) {
+float **pegaMatriz(FILE *arquivo, int n, int m) {
    int x,y;
    float **A;
    A = malloc( n * sizeof (float *));
    int i;
-   for (i = 0; i < n; i++)
-      A[i] = malloc( n * sizeof (float));
+   for (i = 0; i < m; i++)
+      A[i] = malloc( m * sizeof (float));
    
    
-   for (i = 0; i < (n * n); i++){
+   for (i = 0; i < (n * m); i++){
       fscanf(arquivo,"%d %d",&x,&y);
       fscanf(arquivo,"%f",&A[x][y]);
    }
    return A;
 }
 
-void produtoMatriz(float **A,float **B, int n) {
-   int x,y,j,w,total;
-   for (x = 0; x < n; x++) {
-    for (y = 0; y < n; y ++) {
-       total = 0;
-       
-         for (w = 0 ; w < n; w++) {
-          total += A[x][w] * B[w][y];
-         }    
-       printf("%d ",total);
-    }
-    printf("\n");
+float *pegaVetor(FILE *arquivo,int n) {
+   float *b; 
+   int x,i;
+   b = malloc( n * sizeof (float));
+   for (i = 0; i < n; i++) { 
+      fscanf(arquivo,"%d",&x);
+       fscanf(arquivo,"%f",&b[x]);
    }
+   return b;
+}
+
+
+
+
+/*PRONTA para usar no EP3,sqn champs*/
+void rowUperTriang(float **A,float *b, int n) {
+   int i,j;
+   for(i = (n-1); i >= 0; i--) {
+    for(j = (n-1); j >= (i+1); j--)
+      b[i] -= A[i][j] * b[j];
+    if( A[i][i] == 0) { 
+      printf(" SINGULAR ");
+      break;
+    }
+    b[i] *=  pow(A[i][i], -1.0); 
+    
+   }
+}
+
+
+/*Parte do Tonino */
+float **buildQR(float **A, int n, int m) {
+   int k = 0;
+   int i;
+   double r;
+   double y;
+   for(;k < n-1; k++) {
+      r = 0;
+     /*PRIMEIRO PASSO: ENCONTRAR Qk(na verdade u, e y)  
+      /*Encontra, o maior elemento em modulo*/
+      double max = 0;
+      for (i = k; i < n; i++) {
+        if (A[i][k] < 0)
+          A[i][k] *= -1;
+        if (A[i][k] > max)
+          max = A[i][k];
+      }
+      /*-------------------------------------*/ 
+      
+      /*Coluna nula*/
+      if( max == 0) break;
+      /*-----------*/
+      
+      /*Agora divide coluna atula pelo max, para evitar over e under flows*/
+      for (i = k; i < n; i++)
+        A[i][k] /= max;
+      /*------------------------------------------------------------------*/
+      
+      /*Calcua norma dois da coluna da subMatrix Ak*/
+      for (i = k; k < n; k++) 
+         r += (A[i][k] * A[i][k]); 
+      r = sqrt(r);
+      /*-------------------------------------------*/
+      
+      if (A[k][k] < 0) r *= -1;/*Evitar porrivel soma 0*/
+      A[K][K] += r;
+      
+      y =  A[K][K] / r;
+      
+      /*Agora normaliza nosso vetor, dividindo por A[k][k]*/
+      for (i = k + 1; k < n; k++)
+        A[i][k] /= A[K][K];
+      /*--------------------------------------------------*/
+     A[k][k] = 1;
+     r = max * r;
+     /*SEGUNDO PASSO: Efetuar a Q*B indiretamente*/ 
+     
+     
+   }
+}
+
+/*Solução dos quadrados minimos com QR já feita*/
+float minionSqaureSolution(float **R, float *b,int n, int m) {
+  /*Primeiro Monta Qb, para depois aplicarmos */
+  float y;
+  int k;
+  
+  for (k = 0; k < m; k++) {
+   /*y(k)*/
+   y = A[k][k];
+   
+   /*Acha v transposto que é igual u transposto vezes y*/
+   int x;
+   for (x = k + 1; x < m; x++) 
+      A[x][k] = A[k][k]; 
+   
+   /*Agora faz v transposto por b*/
+   float total = 0;
+   for (x = 0; x < n; x++) {
+      if (x > k || x < m)
+        total += A[x][k] + b[x]; 
+      else
+        total += b[x];
+   }
+   /*Agora u vezes total*/
+   for (x = i + 1; x < m; x++) 
+     A[x][i] *= total;
+  
+  /*Constroi nosso vetor v equivalente a b mas na QR*/
+  for (x = 0; x < n; x++) 
+    if (x > i)
+      b[x] -= A[x][i] 
+    else 
+      if (x == i) 
+      b[x] -= 1; 
+  }
+  /*Agora resolvermos sistema triangular superio*/
+  
+
+  
+
+  return 1.0;
 }
